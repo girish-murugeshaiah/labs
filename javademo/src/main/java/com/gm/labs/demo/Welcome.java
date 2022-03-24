da4028a6c1c5ef23decefbe31d4c2cd33ca615bf
girish-murugeshaiah <girishm25@outlook.com>
2022-03-24 00:12:08 -0400

    1.1

--- javademo/src/main/java/com/gm/labs/demo/Welcome.java
+++ javademo/src/main/java/com/gm/labs/demo/Welcome.java
@@ -3,7 +3,7 @@ package com.gm.labs.demo;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to JAVA, GMs");
+		System.out.println("Welcome to JAVA, GM");
 
 	}
 
